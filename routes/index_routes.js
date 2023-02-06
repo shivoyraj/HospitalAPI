ac26e048602a38eb087a0c5865f90a2327ab02af
shivoyraj <shivamrajtripathi@hotmail.com>
2023-02-07 03:40:42 +0530

    added a homepage for info

--- routes/index_routes.js
+++ routes/index_routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const fs = require('fs')
 
 const doctorRoutes = require("./doctor_routes");
 const patientRoutes = require("./patient_routes");
@@ -9,5 +10,14 @@ const reportRoutes = require("./report_routes");
 router.use("/doctors", doctorRoutes);
 router.use("/patients", patientRoutes);
 router.use("/reports", reportRoutes);
+
+router.get("/", (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile('index.html', function (err, data) {
+        if (err) 
+            return res.end('<h1>Error!</h1>')
+        return res.end(data)
+    });
+});
 
 module.exports = router;
